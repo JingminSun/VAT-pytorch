6def66ed6cab7a12b58d14e54a06c93c73460529
JingminSun <jasmine991102@icloud.com>
2023-12-01 00:10:27 -0500

    update bash file

--- run.sh
+++ run.sh
@@ -1,12 +1,12 @@
-#
-#python3 train.py --exp-id all --vat True --dataset MNIST &&
-#
-#python3 train.py --exp-id all --vat True --dataset CIFAR10 &&
-#
-#python3 train.py --exp-id all --vat True --dataset CIFAR100 &&
 
-python3 train.py --exp-id all --vat False --dataset MNIST &&
+python3 train.py --exp-id all --vat True --dataset MNIST &&
 
-python3 train.py --exp-id all --vat False --dataset CIFAR10 &&
+python3 train.py --exp-id all --vat True --dataset CIFAR10 &&
 
-python3 train.py --exp-id all --vat False --dataset CIFAR100
+python3 train.py --exp-id all --vat True --dataset CIFAR100 &&
+
+python3 train.py --exp-id all --dataset MNIST &&
+
+python3 train.py --exp-id all --dataset CIFAR10 &&
+
+python3 train.py --exp-id all --dataset CIFAR100
